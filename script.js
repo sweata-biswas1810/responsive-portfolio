b8562ebfa4030e6ae0a4f1e814f5322456d85d05
sweata-biswas1810 <helpdesk.sweata@gmail.com>
2025-03-31 12:06:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.queryselector('header nav a[href*=' + id + ' ]').classList.add('active')
+            });
+
+           
+        }
+        
+    })
+}
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+}
+document.getElementById("myButton").addEventListener("click", function() {
+    document.getElementById("mySection").scrollIntoView({ behavior: "smooth" });
+  });
+  document.getElementById("btn").addEventListener("click", function() {
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+  });
